1bf8e44d6fac2127eec88e060174476b01d9ea75
Lianne Turnbull <lianne.turnbull@gmail.com>
2022-09-16 16:27:56 +0100

    Set up user registration

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 require("dotenv").config();
 
 mongoose.connect(process.env.MONGO_URL, {
@@ -11,19 +12,39 @@ mongoose.connect(process.env.MONGO_URL, {
 
 const exerciseSchema = new mongoose.Schema({
     description: { type: String, required: true },
-    duration: { type: number, required: true },
+    duration: { type: Number, required: true },
     date: { type: Date, default: Date.now },
 });
 
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true },
-    log: [excercises],
+    log: [exerciseSchema],
 });
+
+const User = mongoose.model("User", userSchema);
+
+const urlEncodedParser = bodyParser.urlencoded({ extended: false });
 
 app.use(cors());
 app.use(express.static("public"));
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/views/index.html");
+});
+app.post("/api/users", urlEncodedParser, async (req, res) => {
+    const username = req.body.username;
+    const saveUser = async () => {
+        try {
+            const user = new User({ username });
+            const savedUser = await user.save();
+            res.json({
+                _id: savedUser._id,
+                username: savedUser.username,
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    };
+    await saveUser();
 });
 
 const listener = app.listen(process.env.PORT || 3000, () => {
