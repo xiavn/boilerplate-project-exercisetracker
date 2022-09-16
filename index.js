6c04e0a2ad5a59c5b6e0ecded52561e0c439a082
Lianne Turnbull <lianne.turnbull@gmail.com>
2022-09-16 15:57:14 +0100

    Set up schema's

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-app.use(cors())
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 });
 
+const exerciseSchema = new mongoose.Schema({
+    description: { type: String, required: true },
+    duration: { type: number, required: true },
+    date: { type: Date, default: Date.now },
+});
 
+const userSchema = new mongoose.Schema({
+    username: { type: String, required: true },
+    log: [excercises],
+});
 
-
+app.use(cors());
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/views/index.html");
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+    console.log("Your app is listening on port " + listener.address().port);
+});
