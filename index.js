a2abfaf1de099eba9c82165312840188325d3d5d
Lianne Turnbull <lianne.turnbull@gmail.com>
2022-09-16 17:54:30 +0100

    Add in ability to add exercise

--- index.js
+++ index.js
@@ -10,15 +10,15 @@ mongoose.connect(process.env.MONGO_URL, {
     useUnifiedTopology: true,
 });
 
-const exerciseSchema = new mongoose.Schema({
-    description: { type: String, required: true },
-    duration: { type: Number, required: true },
-    date: { type: Date, default: Date.now },
-});
-
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true },
-    log: [exerciseSchema],
+    log: [
+        {
+            description: { type: String, required: true },
+            duration: { type: Number, required: true },
+            date: { type: Date, default: Date.now },
+        },
+    ],
 });
 
 const User = mongoose.model("User", userSchema);
@@ -29,6 +29,10 @@ app.use(cors());
 app.use(express.static("public"));
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/views/index.html");
+});
+app.get("/api/users", async (req, res) => {
+    const users = await User.find({}, "username _id").exec();
+    res.json(users);
 });
 app.post("/api/users", urlEncodedParser, async (req, res) => {
     const username = req.body.username;
@@ -45,6 +49,31 @@ app.post("/api/users", urlEncodedParser, async (req, res) => {
         }
     };
     await saveUser();
+});
+app.post("/api/users/:_id/exercises", urlEncodedParser, async (req, res) => {
+    const exercise = {
+        description: req.body.description,
+        duration: req.body.duration,
+        date: req.body.date ? req.body.date : undefined,
+    };
+    const saveExercise = async () => {
+        try {
+            const user = await User.findById(req.params._id).exec();
+            user.log.push(exercise);
+            const lastExercise = user.log[user.log.length - 1];
+            await user.save();
+            res.json({
+                _id: user._id,
+                username: user.username,
+                description: lastExercise.description,
+                duration: lastExercise.duration,
+                date: lastExercise.date.toDateString(),
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    };
+    await saveExercise();
 });
 
 const listener = app.listen(process.env.PORT || 3000, () => {
